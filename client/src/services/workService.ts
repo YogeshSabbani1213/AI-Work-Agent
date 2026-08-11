2cf4ea26e53835cdf6f0b2c5779ce67bc3c53d0d
YogeshSabbani1213 <yogeshsabbani67@gmail.com>
2026-08-11 17:55:39 +0530

    Add work api service

---
+++ client/src/services/workService.ts
@@ -0,0 +1,31 @@
+import api from "../api/api";
+
+// This interface describes the data that our frontend
+// sends to the backend.
+//
+// TypeScript uses this interface to make sure that
+// processWork() receives the correct data structure.
+interface WorkRequest {
+  request: string;
+}
+
+// This interface describes the response we currently
+// expect from our backend.
+interface WorkResponse {
+  success: boolean;
+  message: string;
+  data: {
+    request: string;
+  };
+}
+
+// This function is responsible for calling the
+// POST /api/work endpoint.
+//
+// Keeping API calls inside services means our React
+// components don't need to know endpoint URLs.
+export const processWork = async (data: WorkRequest): Promise<WorkResponse> => {
+  const response = await api.post<WorkResponse>("/work", data);
+    console.log("responseData:",response.data)
+  return response.data;
+};
