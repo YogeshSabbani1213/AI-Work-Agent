728c52181687dee2166d2baf50f9b2bc95a7f4bc
YogeshSabbani1213 <yogeshsabbani67@gmail.com>
2026-08-11 17:42:02 +0530

    Create Common and baseURL

---
+++ client/src/api/api.ts
@@ -0,0 +1,4 @@
+// Base URL of our backend API.
+// Keeping this in one place means we only need to change it once
+// when we deploy our backend.
+export const API_BASE_URL = "http://localhost:5000/api";
