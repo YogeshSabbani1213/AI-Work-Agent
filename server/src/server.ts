4a7c1f1f091349288a10352ecb137d1ea0c9d7ee
YogeshSabbani1213 <yogeshsabbani67@gmail.com>
2026-08-12 16:21:59 +0530

    Add dotnev

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,9 @@
+import dotenv from "dotenv";
 import app from "./app.js";//app.js/ts ?  Because we're using Node's ES module system with:"module": "NodeNext"
+
+// Load environment variables from the .env file.
+dotenv.config();
+
 
 // The port number tells Node.js where our backend server should listen.
 const PORT = 5000;
