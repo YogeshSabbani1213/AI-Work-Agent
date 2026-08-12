15f164cb89b18834c1015144943e22c3ec8266e6
YogeshSabbani1213 <yogeshsabbani67@gmail.com>
2026-08-12 16:28:04 +0530

    Create WorkRequest database table

---
+++ server/prisma/migrations/20260812105350_create_work_request/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE `WorkRequest` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `originalRequest` TEXT NOT NULL,
+    `status` VARCHAR(191) NOT NULL DEFAULT 'pending',
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
